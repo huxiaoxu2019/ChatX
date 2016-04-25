7186f031c82f810355ed7b08814a40757bcae607
genialx <admin@ihuxu.com>
2016-04-26 00:13:39 +0800

    some codes of the chat window

--- src/com/ihuxu/chatx/view/Chat.java
+++ src/com/ihuxu/chatx/view/Chat.java
@@ -2,21 +2,30 @@ package com.ihuxu.chatx.view;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
-public class Chat extends CommonView implements MouseListener{
+public class Chat extends CommonView implements MouseListener, KeyListener{
 
 	private static final long serialVersionUID = -6919521642788139246L;
 
 	JPanel backgroundPanel, topPanel, centerPanel, bottomPanel;
 	
-	JButton closeButton;
+	JButton closeButton, sendButton;
+	
+	JLabel avatarLabel, nickNameLabel;
+	
+	JTextArea inputArea, displayArea;
 	
 	public Chat() {
 		/** background panel **/
@@ -34,6 +43,18 @@ public class Chat extends CommonView implements MouseListener{
 //		this.topPanel.setOpaque(false);
 		this.topPanel.setBackground(new Color(1, 1, 1, 0.5f));
 //		this.topPanel.setBackground(Color.RED);
+		
+		/** avatar **/
+		this.avatarLabel = new JLabel(new ImageIcon("resource/image/avatar_56_56.png"));
+		this.avatarLabel.setBounds(10, 10, 56, 56);
+		this.topPanel.add(this.avatarLabel);
+		
+		/** nickname label **/
+		this.nickNameLabel = new JLabel("GenialX");
+		this.nickNameLabel.setFont(new Font("谐体", Font.BOLD, 16));
+		this.nickNameLabel.setBounds(70, 2, 60, 30);
+		this.nickNameLabel.setForeground(Color.WHITE);
+		this.topPanel.add(this.nickNameLabel);
 		
 		/** close button **/
 		this.closeButton = new JButton(new ImageIcon("resource/image/close_press.png"));
@@ -60,13 +81,32 @@ public class Chat extends CommonView implements MouseListener{
 		
 		/** bottom panel **/
 		this.bottomPanel = new JPanel();
-		this.bottomPanel.setPreferredSize(new Dimension(600, 140));
+		this.bottomPanel.setLayout(null);
 		this.bottomPanel.setBounds(0, 360, 600, 140);
 		this.bottomPanel.setBackground(new Color(1, 1, 1, 0.5f));
 //		this.bottomPanel.setOpaque(false);
 //		this.bottomPanel.setBackground(Color.GRAY);
 		
+		/** input area **/
+		this.inputArea = new JTextArea();
+		this.inputArea.setBounds(0, 0, 600, 100);
+		this.inputArea.addKeyListener(this);
+		this.bottomPanel.add(this.inputArea);
 		this.backgroundPanel.add(this.bottomPanel);
+		
+		/** send button **/
+		this.sendButton = new JButton("发送", new ImageIcon("resource/image/chatframe/send_normal.png"));
+		this.sendButton.setHorizontalTextPosition(JButton.CENTER);
+		this.sendButton.setVerticalTextPosition(JButton.CENTER);
+		this.sendButton.setForeground(Color.WHITE);
+		this.sendButton.setBounds(500, 106, 95, 28);
+		this.sendButton.setContentAreaFilled(false);
+		this.sendButton.setFocusPainted(false);
+		this.sendButton.setBorderPainted(false);
+		this.sendButton.setOpaque(false);
+		this.sendButton.setBorder(null);
+		this.sendButton.addMouseListener(this);
+		this.bottomPanel.add(this.sendButton);
 		
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		this.setLocation(screenSize.width / 2 - 600 /2 , screenSize.height / 2 - 500 / 2);
@@ -112,6 +152,8 @@ public class Chat extends CommonView implements MouseListener{
 			/** close button clicked event **/
 			System.out.println("close button clicked event on the chat window.");
 			this.dispose();
+		} else if(e.getSource() == this.sendButton) {
+			System.out.println("send button clicked event on the chat window.");
 		}
 	}
 
@@ -139,6 +181,37 @@ public class Chat extends CommonView implements MouseListener{
 
 	@Override
 	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == this.inputArea) {
+			switch(e.getKeyCode()) {
+				case KeyEvent.VK_ENTER:
+					System.out.println("recieved the enter key pressed event from inputArea.");
+					this.sendButton.doClick();
+				break;
+				default:
+				break;
+			}
+		}
+		
+	}
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
