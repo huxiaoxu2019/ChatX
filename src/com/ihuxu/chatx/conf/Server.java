d51494d8bc10f115533caafe26e7934c0872d189
GenialX Hoo <genialx@qq.com>
2016-05-03 21:46:56 +0800

    modify the config

--- src/com/ihuxu/chatx/conf/Server.java
+++ src/com/ihuxu/chatx/conf/Server.java
@@ -1,6 +1,6 @@
 package com.ihuxu.chatx.conf;
 
-public class Server {
+public interface Server {
 	public static final String HOST = "115.28.36.19";
-	public static final int PORT = 1720;
+	public static final int PORT = 1722;
 }
